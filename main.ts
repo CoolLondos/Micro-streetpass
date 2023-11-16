15f571cb7bdbab524001c2879400cd07cf086d5c
Londos <londos2021@gmail.com>
2023-11-16 16:27:27 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,18 @@
-datalogger.onLogFull(function () {
-	
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index < 2; index++) {
+        let Score = 0
+        basic.showString("" + (Score))
+    }
 })
-radio.onReceivedString(function (receivedString) {
-	
+radio.onReceivedValue(function (name, value) {
+    if (name.substr(0, 4) == "M-SP") {
+        music.play(music.stringPlayable("E A D F E G F - ", 240), music.PlaybackMode.UntilDone)
+        datalogger.log(datalogger.createCV("Microbits-passed", value))
+    }
 })
-datalogger.setColumnTitles(
-"Score",
-"Microbits-passed"
-)
+datalogger.setColumnTitles("Microbits-passed")
+datalogger.includeTimestamp(FlashLogTimeStampFormat.None)
+let idx = 1
 basic.forever(function () {
-	
+    radio.sendValue("M-SP", control.deviceSerialNumber())
 })
